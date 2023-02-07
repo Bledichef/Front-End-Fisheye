14c1a173acd9104625e9843a6fefcee328771eaa
Guillaume C <81625005+Bledichef@users.noreply.github.com>
2023-02-07 09:46:22 +0100

    Ajout icon modal

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -112,16 +112,194 @@ function displayMediaPhotographers(media) {
 }
 
 
+class Lightbox {
+	
+	//altAttribut = tous les alts
+	//altCurrent = alt de l'image cliquée
+	static init() {
+		// transforme objet en tableau et y met toutes les balises médias de la page(vidéos + photos)
+		const links = Array.from(document.querySelectorAll(".media"));
+		// créé un nouveau tableau et y met tous les liens vers les médias (récupère l'attribut src)
+		const gallery = links.map(link => link.getAttribute("src"));
+		// créé un tableau avec tous les alt
+		const altAttribut = links.map(link => link.getAttribute("alt"));
+
+		const listTitle = links.map(link => link.dataset.title);
+
+
+		//pour chaque balise média on créé une instance de Lightbox
+		links.forEach(link => {
+			link.addEventListener("click", e =>{
+				e.preventDefault();
+				const altCurrent = e.currentTarget.getAttribute("alt"); //récupère le alt de l'image cliquée
+				const titleCurrent = e.currentTarget.dataset.title;
+				new Lightbox(e.currentTarget.getAttribute("src"), gallery, altAttribut, altCurrent,  listTitle, titleCurrent); //récupère l'url de l'image cliquée
+				
+			});	
+
+			link.addEventListener("keydown", e => {
+				if(e.key === "Enter"){
+					e.preventDefault();
+					const altCurrent = e.currentTarget.getAttribute("alt"); //récupère le alt de l'image cliquée
+					const titleCurrent = e.currentTarget.dataset.title;
+					new Lightbox(e.currentTarget.getAttribute("src"), gallery, altAttribut, altCurrent,  listTitle, titleCurrent);
+				}
+			});
+
+		});
+	}
+
+	constructor (url, gallery, altAttribut, altCurrent, listTitle, titleCurrent) {
+		this.element = this.buildDOM(url); // construction du DOM à partir de l'url
+		this.gallery = gallery;		
+		this.altCurrent = altCurrent; //alt de l'image cliquée
+		this.altAttribut = altAttribut; //tous les alts
+		this.titleCurrent = titleCurrent;
+		this.listTitle = listTitle;
+		
+		document.body.appendChild(this.element); // insertion dans le body des éléments
+
+		this.loadMedia(url, altCurrent, titleCurrent);
+		this.onKeyUp = this.onKeyUp.bind(this);
+		document.addEventListener("keyup", this.onKeyUp); // écoute le keyup
+	}
+	
+	/*
+	Créé la balise qui contient les éléments de la ligthbox
+	Permet de charger à l'intérieur les médias, différencie si image ou vidéo et créé les balises adaptées et met dans la balise l'attribut src qui correspond à l'image sur laquelle on a cliqué
+	*/
+	loadMedia(url, altCurrent, titleCurrent) {
+
+		this.url = null;	
+		const container = this.element.querySelector(".media-container");
+		container.innerHTML = "";	
+		this.url = url; //pour cibler l'image
+		this.altCurrent = altCurrent; // pour cibler le alt
+		this.titleCurrent = titleCurrent;
+	
+		if (this.url.includes("jpg")) {
+			const image = new Image(); 
+			let title = document.createElement("p");
+			container.appendChild(image);
+			container.appendChild(title);
+			
+			title.innerHTML = titleCurrent;
+			image.src = url;
+			image.alt = altCurrent; //rajoute le alt ciblé
+			
+		}
+	
+		if (this.url.includes("mp4")) {
+			const video = document.createElement("video");
+			let title = document.createElement("p");
+			container.appendChild(video);
+			container.appendChild(title);
+			title.innerHTML = titleCurrent;
+			video.src = url;
+			video.setAttribute("alt", altCurrent);
+			video.setAttribute("controls", "");
+			title.innerHTML = titleCurrent;
+			video.focus();
+		}
+	}
+	/*
+	méthode qui prend en paramètre un évenement de type Keyboard event = 
+	fermer lightbox avec esc clavier
+	touche fleche droite = media suivant
+	touche fleche gauche = media précédent
+	*/
+	onKeyUp(e) {
+		if (e.key === "Escape") {
+			this.close(e); // si la clé pressée est la touche esc -> appel de la méthode close
+		} else if (e.key === "ArrowLeft") {
+			this.prev(e); // si la clé pressée est la touche fleche gauche-> appel de la méthode prev
+		} else if (e.key === "ArrowRight") {
+			this.next(e); // si la clé pressée est la touche fleche droite -> appel de la méthode next
+		}
+	}
+
+	//méthode qui prend en paramètre un évenement de type mouse event
+	close(e) {
+		e.preventDefault();
+		this.element.parentElement.removeChild(this.element);
+		document.removeEventListener("keyup", this.onKeyUp);
+
+	}
+	//méthode qui prend en paramètre un évenement de type mouse event ou keyboard event
+	next(e) {
+		e.preventDefault();
+		let i = this.gallery.findIndex(image => image === this.url); //pour cibler l'url de l'image
+		let n = this.altAttribut.findIndex(alt => alt === this.altCurrent); //pour cibler le alt
+		let x = this.listTitle.findIndex(title => title === this.titleCurrent);
+
+		if(i === this.gallery.length - 1 ) { //si c'est la dernière image
+			i = -1; // on revient à 0
+		}
+
+		if(n === this.altAttribut.length - 1){ //si dernier alt
+			n = -1;
+		}
+
+		if(x === this.listTitle.length - 1) {
+			x = -1;
+		}
+
+		this.loadMedia(this.gallery[i + 1], this.altAttribut[n + 1], this.listTitle[x + 1]);
+	}
+
+	//méthode qui prend en paramètre un évenement de type mouse event ou keyboard event
+	prev(e) {
+		e.preventDefault();
+		let i = this.gallery.findIndex(image => image === this.url);
+		let n = this.altAttribut.findIndex(alt => alt === this.altCurrent);
+		let x = this.listTitle.findIndex(title => title === this.titleCurrent);
+
+		if(i === 0 ) { // si c'est la première image
+			i = this.gallery.length; // on passe à la dernière image
+		}
+
+		if(n === 0) {
+			n = this.altAttribut.length;
+		}
+
+		if(x === 0){
+			x = this.listTitle.length;
+		}
+
+		this.loadMedia(this.gallery[i - 1], this.altAttribut[n - 1], this.listTitle[x - 1]);
+	}
+
+	// création des éléments HTML + return 
+	buildDOM (url) {
+		const domLightbox = document.createElement("section");
+		domLightbox.classList.add("lightbox");
+		domLightbox.setAttribute("aria-label", "image closeup view");
+		domLightbox.setAttribute("aria-hidden", "false");
+		domLightbox.innerHTML = 
+			`<button type="button" aria-label="Close dialog" class="lightbox-close"><i class="fa-solid fa-xmark"></i></button>
+			<button type="button" aria-label="Next image" class="lightbox-next"><i class="fa-sharp fa-solid fa-chevron-right"></i></button>
+			<button type="button" aria-label="Previous image" class="lightbox-prev"><i class="fa-sharp fa-solid fa-chevron-left"></button>
+			<div class="media-container" aria-label="media open view"></div>
+			`;
+
+		domLightbox.querySelector(".lightbox-close").addEventListener("click", this.close.bind(this));
+		domLightbox.querySelector(".lightbox-next").addEventListener("click", this.next.bind(this));
+		domLightbox.querySelector(".lightbox-prev").addEventListener("click", this.prev.bind(this));
+		return domLightbox;
+	}
+}
+
 async function init() {
 	// Récupère les datas des photographes
 	const { photographers } = await getElementsPhotographers();
 	const { media } = await getMediaPhotographers();
 	displayDataPhotographers(photographers);
 	displayMediaPhotographers(media);
+	Lightbox.init();
 //	cardLikesAndPrice(photographers, media);
 //	addLike(media);
-	sort(media);
-//	Lightbox.init();//
+//	sort(media);
+	
 
 }
 
